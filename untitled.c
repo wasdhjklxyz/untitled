7c9438a99bf49e049c0a1ec6b0ac473746c83df2
uiop <uiop@wasdhjkl.xyz>
2025-10-01 22:00:21 -0400

    feat: add file operations

--- untitled.c
+++ untitled.c
@@ -2,10 +2,47 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+#include "untitled.h"
+
 MODULE_LICENSE("GPL-2.0");
 
 dev_t dev;
 int nr_devs = 4;
+
+static const struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.llseek = generic_file_llseek,
+	.read = untitled_read,
+	.write = untitled_write,
+	.open = untitled_open,
+	.release = untitled_release,
+};
+
+ssize_t untitled_read(struct file *filp, char __user *buf, size_t count,
+		      loff_t *pos)
+{
+	printk(KERN_DEBUG "untitled: read");
+	return 0;
+}
+
+ssize_t untitled_write(struct file *filp, const char __user *buf, size_t count,
+		       loff_t *pos)
+{
+	printk(KERN_DEBUG "untitled: write");
+	return 0;
+}
+
+int untitled_open(struct inode *inode, struct file *filp)
+{
+	printk(KERN_DEBUG "untitled: open");
+	return 0;
+}
+
+int untitled_release(struct inode *inode, struct file *filp)
+{
+	printk(KERN_DEBUG "untitled: release");
+	return 0;
+}
 
 static int hello_init(void)
 {
@@ -16,6 +53,7 @@ static int hello_init(void)
 		printk(KERN_WARNING "Error: alloc_chrdev_region\n");
 	}
 
+	// TODO: Wait for chrdev files to be created?
 	return ret;
 }
 
---
+++ untitled.h
@@ -0,0 +1,15 @@
+#ifndef _UNTITLED_H
+#define _UNTITLED_H
+
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/compiler_types.h>
+
+ssize_t untitled_read(struct file *filp, char __user *buf, size_t count,
+		      loff_t *pos);
+ssize_t untitled_write(struct file *filp, const char __user *buf, size_t count,
+		       loff_t *pos);
+int untitled_open(struct inode *inode, struct file *filp);
+int untitled_release(struct inode *inode, struct file *filp);
+
+#endif
