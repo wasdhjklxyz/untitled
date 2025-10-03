a7d735f2b53210d8b0fe91e40acb469689e52f62
uiop <uiop@wasdhjkl.xyz>
2025-10-03 15:17:52 -0400

    feat: add device structure

--- untitled.h
+++ untitled.h
@@ -4,8 +4,20 @@
 #include <linux/types.h>
 #include <linux/fs.h>
 #include <linux/compiler_types.h>
+#include <linux/semaphore.h>
+#include <linux/cdev.h>
 
 struct cdev *untitled_cdev;
+
+struct untitled_dev {
+	struct untitled_qset *data;
+	int quantum;
+	int qset;
+	unsigned long size;
+	unsigned int access_key;
+	struct semaphore sem;
+	struct cdev cdev;
+};
 
 ssize_t untitled_read(struct file *filp, char __user *buf, size_t count,
 		      loff_t *pos);
